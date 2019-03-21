c251fc802f9483791e67ab6c00243c78e7e9cc18
Andrei Malaev <andrey1391@mail.ru>
2019-03-21 18:16:59 +0300

    Add some stuff

--- G.hs
+++ G.hs
@@ -6,6 +6,9 @@ module G
 	nudge
 ) where
 
+import qualified Data.Map as Map
+import Data.Maybe (fromJust)
+
 data Point = Point Float Float deriving (Show)
 data Shape = Circle Point Float | Rectangle Point Point deriving (Show)
 
@@ -60,26 +63,88 @@ vScalar :: (Num a) => Vector a -> Vector a -> Vector a
 data Day = Mon | Tue | Wed | Thu | Fri | Sat | Sun
 	deriving (Show, Eq, Ord, Read, Bounded, Enum)
 
--- Type synonyms ...
+type Ints = [Int]
+type PhoneNumber = String
+type Name = String
+type PhoneBook = [(Name, PhoneNumber)]
 
+type AssocList k v = [(k, v)]
+type IntMap = Map.Map Int
 
+isInPhoneBook :: Name -> PhoneNumber -> PhoneBook -> Bool
+isInPhoneBook name pNumber pBook = (name, pNumber) `elem` pBook
 
+data Failure = Failure {
+	reason :: String,
+	code :: Int
+} deriving (Show)
 
+apiCall :: String -> Either Failure String
+apiCall "ok" = Right "allOk"
+apiCall x = Left $ Failure "something wrong" 1488
 
+data LockerState = Taken | Free deriving (Show, Eq)
+type ErrorMessage = String
+type Code = String
+type Lockers = Map.Map Int (LockerState, Code)
 
+-- locker can be taken, locker can be not existing
 
+lockers :: Lockers
+lockers = Map.fromList[
+    	(100,(Taken,"ZD39I")),
+    	(101,(Free,"JAH3I")),
+    	(103,(Free,"IQSA9")),
+    	(105,(Free,"QOTSA")),
+    	(109,(Taken,"893JJ")),
+    	(110,(Taken,"99292"))
+    	]
 
+lockerLookup :: Int -> Lockers -> Either ErrorMessage Code
+lockerLookup x xs = case Map.lookup x xs of
+	Nothing -> Left "no such locker, succ"
+	Just (state, code) -> if (state == Taken)
+			then Left "thas takennn!"
+			else Right code
 
+-- todo uncomment
+-- data List a = Empty | Cons a (List a) deriving (Show, Read, Eq, Ord)
+-- data ListA a = EmptyA | ConsA { -- same as the top one
+-- 	headA :: a,
+-- 	tailA :: List a
+-- } deriving (Show, Read, Eq, Ord)
 
+infixr 5 :-:
+data List a = Empty | a :-: List a deriving (Show, Read, Eq, Ord)
 
+infixr 5 $$ 
+($$) :: List a -> List a -> List a
+Empty $$ ys = ys
+(x :-: xs) $$ ys = x :-: (xs $$ ys)
 
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show, Read, Eq) 
 
+singleton :: a -> Tree a
+singleton x = Node x EmptyTree EmptyTree
 
+insertTree :: (Ord a) => a -> Tree a -> Tree a
+insertTree x EmptyTree = singleton x
+insertTree x (Node y left right)
+	| x == y = Node y left right
+	| x < y = Node y (insertTree x left) right
+	| x > y = Node y left (insertTree x right)
 
+treeElem :: (Ord a) => a -> Tree a -> Bool
+treeElem _ EmptyTree = False
+treeElem x (Node y left right)
+	| x == y = True
+	| x < y = treeElem x left 
+	| x > y = treeElem x right
 
+-- foldl treeInsert EmptyTree [1,2,3,4,5,6,8]
+numsTree = foldr insertTree EmptyTree [1,2,3,4,5,6,8]
 
-
-
+-- Typeclasses 102
 
 
 
